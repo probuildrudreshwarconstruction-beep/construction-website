c4320aebd00cc5a93097bee2d3f142b401a766cb
probuildrudreshwarconstruction-beep <sidyy001@gmail.com>
2025-10-21 00:57:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -264,7 +264,7 @@ st.markdown(f"""
 
 # -------------------- Admin Panel --------------------
 st.markdown("<hr><h2></h2><hr>", unsafe_allow_html=True)
-if st.button("🔒"):
+if st.button(""):
     st.session_state.admin_visible = not st.session_state.admin_visible
 
 if st.session_state.admin_visible:
